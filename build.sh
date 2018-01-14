e0030bea4a538bcff0103f9cecbac2e96bde1e89
Mikołaj Przygoda <przygoda.mikolaj@gmail.com>
2018-01-14 20:34:28 +0100

    Additional option in build.sh so that the executable has suitable name.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #You must have pyinstaller (duh) and pygame to build the project
-pyinstaller main.py -F --add-data assets:assets
+pyinstaller main.py -F --add-data assets:assets -n Space\ Invaders
